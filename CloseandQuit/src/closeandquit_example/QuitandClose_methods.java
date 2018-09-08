de15a04e23659cc4254a127547823a6de8c4af2d
prateek agarwal <prateek agarwal@LAPTOP-TUUPRPNM>
2018-09-08 17:11:55 +0530

    new line change

--- CloseandQuit/src/closeandquit_example/QuitandClose_methods.java
+++ CloseandQuit/src/closeandquit_example/QuitandClose_methods.java
@@ -18,7 +18,7 @@ public class QuitandClose_methods {
 		driver.findElement(By.xpath("html/body/div[1]/div/div/div/div[1]/ul/li[2]/a")).click();
 		Thread.sleep(3000);
 		driver.findElement(By.xpath("//*[@id='Seperate']/button")).click();
-		Thread.sleep(2000);
+		Thread.sleep(1000);
 		driver.close();
 		driver.quit();
 				
