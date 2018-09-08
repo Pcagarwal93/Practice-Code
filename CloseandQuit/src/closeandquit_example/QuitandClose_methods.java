102d7979f9a0a7a7d838fe8146f22af087762265
prateek agarwal <prateek agarwal@LAPTOP-TUUPRPNM>
2018-09-08 17:09:51 +0530

    this is for practice purpose.

---
+++ CloseandQuit/src/closeandquit_example/QuitandClose_methods.java
@@ -0,0 +1,27 @@
+package closeandquit_example;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class QuitandClose_methods {
+
+	public static void main(String[] args) throws InterruptedException {
+		
+		System.setProperty("webdriver.gecko.driver", "C:\\Users\\prateek agarwal\\workspace\\CloseandQuit\\geckodriver.exe");
+		WebDriver driver= new FirefoxDriver();
+		driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
+		driver.get("http://demo.automationtesting.in/Windows.html");
+		
+		driver.findElement(By.xpath("html/body/div[1]/div/div/div/div[1]/ul/li[2]/a")).click();
+		Thread.sleep(3000);
+		driver.findElement(By.xpath("//*[@id='Seperate']/button")).click();
+		Thread.sleep(2000);
+		driver.close();
+		driver.quit();
+				
+	}
+	
+}
